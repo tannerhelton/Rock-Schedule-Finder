ef22bf7f174fbaa7a4cf0a41044b99de1d3ab64e
tannerwh <tannerwh@gmail.com>
2016-11-09 09:56:54 -0600

    Added types of search

--- Rock Schedule Finder/Rock Schedule Finder/SearchViewController.swift
+++ Rock Schedule Finder/Rock Schedule Finder/SearchViewController.swift
@@ -10,26 +10,27 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchType: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
-
+    
+    @IBAction func searchTypeAction(_ sender: Any?) {
+        if (searchType.selectedSegmentIndex == 0) {
+            //Show date selection
+        } else if (searchType.selectedSegmentIndex == 1) {
+            //Show day type search
+        }
+    }
 }
