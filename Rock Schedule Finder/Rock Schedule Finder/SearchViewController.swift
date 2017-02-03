d2a4eaf9509899f2dced78b5a3574e5ac17e5a63
Tanner Helton <tannerwh@gmail.com>
2017-02-03 11:34:25 -0600

    Added alamofire and swiftjson and go calendar to work

--- Rock Schedule Finder/Rock Schedule Finder/SearchViewController.swift
+++ Rock Schedule Finder/Rock Schedule Finder/SearchViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class SearchViewController: UIViewController {
     
@@ -18,6 +20,27 @@ class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let geturl = "https://www.googleapis.com/calendar/v3/calendars/rockhursths.edu_vm6bt9h5uust4qkr1tqnkmtnac@group.calendar.google.com/events?singleEvents=true&orderBy=startTime&sortOrder=ascending&timeMin=2017-01-01T00:00:00-05:00&timeMax=2017-04-12T00:00:00-05:00&key=AIzaSyBlp8z5roe3qRkLqngQvEhRvS6pes7u82g"
+        
+        
+        Alamofire.request(geturl , method: .get, encoding: JSONEncoding(options: [])).responseJSON { response in
+            debugPrint(response)
+            
+            if let result = response.result.value {
+                let json = JSON(result as! NSDictionary)
+                let tasks = json["items"].arrayValue
+                
+                // display only the last task
+                let subject = tasks[tasks.count - 1]["0"].string
+                let description = tasks[tasks.count - 1]["summary"].string
+                print(subject)
+                print(description)
+                print("This is it: \(description)")
+            } else {
+                print("Error")
+            }
+        }
         
     }
     
